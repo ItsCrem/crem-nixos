691213016185df543e1aaf20b8c240cfe14ecb26
Crem <maxcaminer1@gmail.com>
2024-05-27 01:48:44 +0000

    first commit for helps

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "crem's nix :)";
+
+  inputs = {
+    # Nixpkgs
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+
+
+    # Home manager
+    home-manager.url = "github:nix-community/home-manager/release-23.11";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    home-manager,
+    ...
+  } @ inputs: let
+    inherit (self) outputs;
+  in {
+
+    # Custom Packags and modfications as overlays
+    overlays = import ./overlays {inherit inputs;};
+    
+    # NixOS configuration entrypoint
+    # Available through 'nixos-rebuild --flake .#your-hostname'
+    nixosConfigurations = {
+      nixos = nixpkgs.lib.nixosSystem {
+        specialArgs = {inherit inputs outputs;};
+        # > Our main nixos configuration file <
+        modules = [./nixos/configuration.nix];
+      };
+    };
+
+    # Standalone home-manager configuration entrypoint
+    # Available through 'home-manager --flake .#your-username@your-hostname'
+    homeConfigurations = {
+      "crem@nixos" = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
+        extraSpecialArgs = {inherit inputs outputs;};
+        # > Our main home-manager configuration file <
+        modules = [./home-manager/home.nix];
+      };
+    };
+  };
+}
---
+++ home-manager/home.nix
@@ -0,0 +1,133 @@
+# This is your home-manager configuration file
+# Use this to configure your home environment (it replaces ~/.config/nixpkgs/home.nix)
+{
+  inputs,
+  outputs,
+  lib,
+  config,
+  pkgs,
+  ...
+}: {
+  # You can import other home-manager modules here
+  imports = [
+    # If you want to use home-manager modules from other flakes (such as nix-colors):
+    # inputs.nix-colors.homeManagerModule
+
+    # You can also split up your configuration and import pieces of it here:
+    # ./nvim.nix
+  ];
+
+  nixpkgs = {
+    # You can add overlays here
+    overlays = [
+    	outputs.overlay.unstable-packages
+      # If you want to use overlays exported from other flakes:
+      # neovim-nightly-overlay.overlays.default
+
+      # Or define it inline, for example:
+      # (final: prev: {
+      #   hi = final.hello.overrideAttrs (oldAttrs: {
+      #     patches = [ ./change-hello-to-hi.patch ];
+      #   });
+      # })
+    ];
+    # Configure your nixpkgs instance
+    config = {
+      # Disable if you don't want unfree packages
+      allowUnfree = true;
+      # Workaround for https://github.com/nix-community/home-manager/issues/2942
+      allowUnfreePredicate = _: true;
+    };
+  };
+
+  home = {
+    username = "crem";
+    homeDirectory = "/home/crem";
+  };
+
+  # Add stuff for your user as you see fit:
+  # programs.neovim.enable = true;
+  home.packages = with pkgs; [
+	###
+	# General
+	###
+
+	firefox
+	neovim
+	neofetch
+	fzf
+	python3
+	go
+	rustup
+	zsh
+	krb5
+	docker
+
+	###
+	# Internal
+	###
+	python311Packages.impacket
+	python311Packages.msldap
+	python311Packages.lsassy
+	python311Packages.pypykatz
+	ldapdomaindump
+	mitm6
+	certipy
+	responder
+	unstable.netexec
+	#unstable.adidnsdump
+	evil-winrm
+	bloodhound-py
+	fping
+	nmap
+	smbmap
+	samba
+	kerbrute
+	coercer
+	#autobloody
+
+
+	###
+	# External
+	###
+
+	sqlmap
+	ffuf
+	gobuster
+	nuclei
+	httpx
+	padre
+	gau
+	masscan
+	#rdwatool
+	# burpsuite-professional
+	# trevorspray
+	# cewler
+	# shortscan
+	
+	###
+	# Wi-Fi
+	###
+
+	wireshark
+	#tshark
+	airgeddon
+	aircrack-ng
+	bettercap
+	#ettercap
+	mdk4
+	hcxdumptool
+	hcxtools
+	# dhcpd
+
+  ];
+
+  # Enable home-manager and git
+  programs.home-manager.enable = true;
+
+  # Nicely reload system units when changing configs
+  systemd.user.startServices = "sd-switch";
+
+  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+  home.stateVersion = "23.11";
+}
