751d786a6cb3e56350c3e685e70a54fca000f74d
Crem <maxcaminer1@gmail.com>
2024-07-14 22:05:14 +1000

    added vscode configuration

--- modules/general/vscode.nix
+++ modules/general/vscode.nix
@@ -2,9 +2,24 @@
 inputs,
 outputs,
 lib,
+pkgs,
 ...
 }:{
 	programs.vscode = {
 		enable = true;
+		package = pkgs.vscode;
+		extensions = with pkgs.vscode-extensions; [
+			ms-python.python
+			catppuccin.catppuccin-vsc
+			esbenp.prettier-vscode
+			pkief.material-icon-theme
+			jnoortheen.nix-ide
+		];
+		userSettings = {
+		 	"workbench.iconTheme" = "material-icon-theme";
+		 	"workbench.colorTheme" = "Catppuccin Mocha Bordered";
+		 	"terminal.integrated.defaultProfile.linux" = "zsh";
+		 	"nix.enableLanguageServer" = true;
+		};	
   	};
 }
